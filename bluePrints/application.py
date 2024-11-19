daed2d435967af344ad4eea7427d5cf1f0e1c7e1
charlie-bu <15947513567@163.com>
2024-11-20 00:32:33 +0800

    chore

--- bluePrints/application.py
+++ bluePrints/application.py
@@ -1,5 +1,4 @@
 from flask import Blueprint, request, jsonify
-from pandas.io.json import to_json
 
 from models import *
 
@@ -27,8 +26,9 @@ def get_all_posts():
         "status": 200,
     })
 
-@bp.route('/get_special_post', methods=['POST'])
-def get_special_post():
+
+@bp.route('/get_this_post', methods=['POST'])
+def get_this_post():
     post_id = request.json['id']
     post = Post.query.get(post_id)
     return jsonify({
@@ -36,6 +36,7 @@ def get_special_post():
         "message": "success",
         "status": 200,
     })
+
 
 @bp.route('/like_post', methods=['POST'])
 def like_post():
@@ -58,10 +59,11 @@ def cancel_like_post():
         "status": 200,
     })
 
+
 @bp.route('/get_post_comments', methods=['POST'])
 def get_post_comments():
-    post = Post.query.get(request.json['post_id'])
-    comments = post.comments
+    data = request.json
+    comments = Post_comment.query.filter_by(post_id=data['post_id']).order_by(Post_comment.time.desc()).all()
     comments = [Post_comment.to_json(comment) for comment in comments]
     return jsonify({
         "comments": comments,
@@ -69,10 +71,11 @@ def get_post_comments():
         "status": 200,
     })
 
+
 @bp.route('/send_comment', methods=['POST'])
 def send_comment():
     data = request.json
-    new_comment = Post_comment(content=data['content'], sender_id=data['my_id'], post_id=data['post_id'])
+    new_comment = Post_comment(content=data['content'], sender_id=data['my_id'], post_id=data['post_id'], likes=0)
     db.session.add(new_comment)
     db.session.commit()
     return jsonify({
@@ -81,14 +84,58 @@ def send_comment():
     })
 
 
-@bp.route('/get_all_post_comments', methods=['POST'])
-def get_all_post_comments():
-    data = request.json
-    post_id = data['post_id']
-    post_comments = Post_comment.query.filter_by(post_id=post_id).order_by(Post_comment.time.desc()).all()
-    post_comments = [Post_comment.to_json(comment) for comment in post_comments]
+@bp.route('/like_comment', methods=['POST'])
+def like_comment():
+    comment = Post_comment.query.get(request.json['comment_id'])
+    comment.likes += 1
+    db.session.commit()
     return jsonify({
-        "post_comments": post_comments,
         "message": "success",
         "status": 200,
     })
+
+
+@bp.route('/cancel_like_comment', methods=['POST'])
+def cancel_like_comment():
+    comment = Post_comment.query.get(request.json['comment_id'])
+    comment.likes -= 1
+    db.session.commit()
+    return jsonify({
+        "message": "success",
+        "status": 200,
+    })
+
+@bp.route('/delete_post', methods=['POST'])
+def delete_post():
+    data = request.json
+    post = Post.query.get(data['post_id'])
+    if post.poster_id != data["my_id"]:
+        return jsonify({
+            "message": "fail: unauthorized",
+            "status": -1,
+        })
+    db.session.delete(post)
+    db.session.commit()
+    return jsonify({
+        "message": "success",
+        "status": 200,
+    })
+
+
+@bp.route('/delete_comment', methods=['POST'])
+def delete_comment():
+    data = request.json
+    comment = Post_comment.query.get(data['comment_id'])
+    if comment.sender_id != data["my_id"]:
+        return jsonify({
+            "message": "fail: unauthorized",
+            "status": -1,
+        })
+    db.session.delete(comment)
+    db.session.commit()
+    return jsonify({
+        "message": "success",
+        "status": 200,
+    })
+
+
