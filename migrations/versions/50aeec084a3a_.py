4f2fab97e126fedb116f1726e4f6a7682f73e01f
Charlie.BU <15947513567@163.com>
2025-07-13 12:47:50 +0800

    add-gitignore

---
+++ migrations/versions/50aeec084a3a_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 50aeec084a3a
+Revises: 
+Create Date: 2025-01-17 22:16:26.645330
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '50aeec084a3a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
